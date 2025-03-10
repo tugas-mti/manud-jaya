b803ef37b9d660328d5d10e63595a51d07f0fa0f
rizqee <arizqee@gmail.com>
2025-03-10 21:22:51 +0700

    add navbar and background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import Navigation from './components/navigation';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navigation/>
+        {children}
+      </body>
     </html>
   );
 }
