205ff6d35c1b9ddb93c7c4f394fc421e36752d62
Condro Wiyono <condro@outlook.co.id>
2025-03-12 21:48:20 +0700

    chore: fix build

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,24 @@ export default function Home() {
     <>
       <div className="w-full ">
         <div className="relative">
-          <div className="relative w-full mx-auto overflow-hidden 
-                    h-[200px] md:h-[100vh]">
-          <Image
+          <div className="relative w-full mx-auto overflow-hidden h-[200px] md:h-[100vh]">
+            <Image
               src="/images/background1.jpg"
               alt="BG"
               layout="fill"
               className="relative"
               objectFit="cover"
-              objectPosition="center" 
+              objectPosition="center"
             />
           </div>
-            <h1 className="absolute inset-0 flex justify-center text-white font-bold top-[10%] text-[3vw]">
-              Welcome to Manud Jaya Tourism Village
-            </h1>
-            <p className="absolute inset-0 flex text-center top-[20%] mx-[30%] text-white text-[1.5vw]  leading-tight">
-              Manud Jaya: The Hidden Paradise That Offers Breathtaking Natural Beauty and a Tranquil Atmosphere
-            </p>
-            <SearchBar />
+          <h1 className="absolute inset-0 flex justify-center text-white font-bold top-[10%] text-[3vw]">
+            Welcome to Manud Jaya Tourism Village
+          </h1>
+          <p className="absolute inset-0 flex text-center top-[20%] mx-[30%] text-white text-[1.5vw]  leading-tight">
+            Manud Jaya: The Hidden Paradise That Offers Breathtaking Natural
+            Beauty and a Tranquil Atmosphere
+          </p>
+          <SearchBar />
         </div>
         <div className="h-screen bg-white"></div>
       </div>
