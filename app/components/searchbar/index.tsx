75b454cf1944d6c237df1095f82e92feeb4da972
rizqee <arizqee@gmail.com>
2025-03-14 22:50:16 +0700

    fix some minor bugs

--- app/components/searchbar/index.tsx
+++ app/components/searchbar/index.tsx
@@ -2,15 +2,15 @@ import { Search } from "lucide-react";
 
 export default function SearchBar() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center mt-[100px] sm:mt-[40vh]">
-        <div className="flex w-[60%] bg-white rounded-lg shadow-lg overflow-hidden h-[2vh] sm:h-[6vh]">
+    <div className="absolute inset-0 flex items-center justify-center mt-[100px] md:mt-[40vh]">
+        <div className="flex w-[60%] bg-white rounded-lg shadow-lg overflow-hidden h-[2vh] md:h-[6vh]">
             <input
             type="text"
             placeholder="What are you looking for?"
-            className="w-full px-[2vw] py-[1vh] sm:px-[3vw] sm:py-[1.5vh] text-gray-700 text-[1vh] sm:text-[1.2vw] outline-none"
+            className="w-full px-[2vw] py-[1vh] md:px-[3vw] md:py-[1.5vh] text-gray-700 text-[1vh] md:text-[1.2vw] outline-none"
             />
             <button className="px-[2vw] bg-gray-200 text-gray-600 hover:bg-gray-300 transition">
-            <Search className="w-[3vw] h-[3vw]" />
+            <Search className="w-[2vh] md:w-[3vh] h-[2vh] md:h-[3vh]" />
             </button>
         </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <>
       <div className="w-full ">
-        <div className="relative">
           <div className="relative w-full mx-auto overflow-hidden h-[200px] md:h-[100vh]">
             <Image
               src="/images/background1.webp"
@@ -17,16 +16,15 @@ export default function Home() {
               objectFit="cover"
               objectPosition="center"
             />
+            <h1 className="absolute inset-0 flex justify-center text-white font-bold top-[10px] md:top-[10%] text-[3vw]">
+              Welcome to Manud Jaya Tourism Village
+            </h1>
+            <p className="absolute inset-0 flex text-center top-[40px] md:top-[20%] mx-[30%] text-white text-[1.5vw]  leading-tight">
+              Manud Jaya: The Hidden Paradise That Offers Breathtaking Natural
+              Beauty and a Tranquil Atmosphere
+            </p>
+            <SearchBar />
           </div>
-          <h1 className="absolute inset-0 flex justify-center text-white font-bold top-[10%] text-[3vw]">
-            Welcome to Manud Jaya Tourism Village
-          </h1>
-          <p className="absolute inset-0 flex text-center top-[20%] mx-[30%] text-white text-[1.5vw]  leading-tight">
-            Manud Jaya: The Hidden Paradise That Offers Breathtaking Natural
-            Beauty and a Tranquil Atmosphere
-          </p>
-          <SearchBar />
-        </div>
         <div className="h-screen bg-white">
           <Content header={header} text={text} image="/images/image1.jpg"/>
         </div>
