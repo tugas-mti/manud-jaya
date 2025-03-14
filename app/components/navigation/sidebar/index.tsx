a8917e71fe2766650f248a0bc9d76c33fc4d442e
rizqee <arizqee@gmail.com>
2025-03-14 11:45:39 +0700

    update so that login looks good

--- app/components/navigation/sidebar/index.tsx
+++ app/components/navigation/sidebar/index.tsx
@@ -13,14 +13,13 @@ const Sidebar = ({
   return (
     <>
       <div
-        className="sidebar-container fixed w-full h-full overflow-hidden justify-center bg-white grid pt-[120px] left-0 z-10"
+        className="sidebar-container fixed w-full h-full overflow-hidden justify-center bg-white pt-[120px] left-0 z-10"
         style={{
           opacity: `${isOpen ? "1" : "0"}`,
           top: ` ${isOpen ? "0" : "-100%"}`,
         }}
       >
-        <button className="absolute right-0 p-5" onClick={toggle}>
-          {/* Close icon */}
+        {/* <button className="absolute right-0 p-5" onClick={toggle}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="48"
@@ -32,7 +31,7 @@ const Sidebar = ({
               d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
             />
           </svg>
-        </button>
+        </button> */}
 
         <ul className="sidebar-nav text-center leading-relaxed text-xl">
           <li>
@@ -55,8 +54,16 @@ const Sidebar = ({
               <p>Contacts</p>
             </Link>
           </li>
+          <li>
+            <div className="flex justify-center">
+              <button onClick={toggle}>
+                <UserProfile />
+              </button>
+            </div>
+            
+          </li>
         </ul>
-        <UserProfile />
+        
       </div>
     </>
   );
